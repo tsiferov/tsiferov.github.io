7b46571998fbf5b37e505b8863e5ad031c8c6798
Tsiferov Serhii <tsiferov@ukr.net>
2016-03-22 16:58:34 +0200

    delete title

--- homeWorks_8/js/script.js
+++ homeWorks_8/js/script.js
@@ -6,13 +6,11 @@ $(function () {
         $(this).addClass('tabsItem--activ').siblings().removeClass('tabsItem--activ');
         $($('.tabsItem--activ a').attr('href')).addClass('tabsContent--visibl').siblings().removeClass('tabsContent--visibl');
     });
-    $('.formItem input').hover(function (e) {
-            e.preventDefault();
-            $(this).siblings('p').show();
-
+    $('.formItem input').hover(function (event) {
+            $(this).siblings('div').show();
         },
         function () {
-            $(this).siblings('p').hide()
+            $(this).siblings('div').hide();
         }
     );
 });

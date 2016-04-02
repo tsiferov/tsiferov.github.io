f6fa63748de3b1dee633b64714baedef30847d61
Tsiferov Serhii <tsiferov@ukr.net>
2016-04-02 23:05:37 +0300

    create

---
+++ homeWorks_12/js/script.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+
+    $('.carouselBox').carouselPlag();
+
+    $('.box').myCarousel({ delay: '3000' });
+
+    $('#buttonJohnResig').on('click', function(){
+        myWindow = open("templatePages/templateJohnResig.html");
+    })
+
+    $('#buttonLoDash').on('click', function(){
+        myWindow = open("templatePages/templateLoDash.html");
+    })
+
+});
+
+

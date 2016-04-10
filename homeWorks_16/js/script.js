9adf715eeeee291a8b8421abde0b57fcf7299ace
Tsiferov Serhii <tsiferov@ukr.net>
2016-04-10 11:42:55 +0300

    +enter

--- homeWorks_16/js/script.js
+++ homeWorks_16/js/script.js
@@ -3,9 +3,8 @@
  */
 
 var responseObj;
-/*var userRequest;*/
 
-function fCallback(jqueryObj, data){
+function fCallback(jqueryObj, data) {
 
     console.log(data);
 
@@ -14,24 +13,32 @@ function fCallback(jqueryObj, data){
     var template = _.template($('#pattern').html());
     var content = template(responseObj);
 
-    if(responseObj.length === 0 ){
-        content =  '<div class="responseBox"><p class="content">На Ваш запит не знайдено жодного документа.</p></div>'
+    if (responseObj.length === 0) {
+        content = '<div class="responseBox"><p class="content">На Ваш запит не знайдено жодного документа.</p></div>'
     }
 
     $('main').empty().append(content);
 }
 
-$(function(){
+$(function () {
 
-  $("#searchButton").on("click",function(){
+    function sendRequest() {
 
-      var userRequest = $("#searchInput").val();
-    console.log(userRequest);
+        var userRequest = $("#searchInput").val();
+        console.log(userRequest);
 
-      $.ajax({
-          url: "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&rsz=large&q="+userRequest+"&callback=fCallback&context=?",
-          dataType: "jsonp",
-      });
+        $.ajax({
+            url: "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&rsz=large&q=" + userRequest + "&callback=fCallback&context=?",
+            dataType: "jsonp",
+        });
+    }
 
-  });
+    $("#searchButton").on("click", sendRequest);
+
+    $("#searchInput").on("keypress", function (event) {
+        if (event.which == 13) {
+            event.preventDefault();
+            sendRequest();
+        }
+    });
 });

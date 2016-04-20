83bc38aa08f12499b1d86022403ea879c2a60fae
Tsiferov Serhii <tsiferov@ukr.net>
2016-04-20 15:39:58 +0300

    change gruntfile

--- homeWorks_18/Gruntfile.js
+++ homeWorks_18/Gruntfile.js
@@ -1,34 +1,34 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.initConfig({
-    concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: ['js/src/*.js'],
-        dest: 'js/script.js'
-      }
-    },
-	uglify: {
-	 dist: {
-           src: ['js/script.js'],
-           dest: 'js/script.min.js'
-      	  }
-	},
-	cssmin: {
+    grunt.initConfig({
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['js/src/*.js'],
+                dest: 'js/script.js'
+            }
+        },
+        uglify: {
+            dist: {
+                src: ['js/script.js'],
+                dest: 'js/script.min.js'
+            }
+        },
+        cssmin: {
             css: {
                 src: ['css/src/*.css'],
                 dest: 'css/style.min.css'
             }
         }
 
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
+    grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
 
 };
